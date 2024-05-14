1f008e50c471c7ecc8e9a116acb04e314853bde6
Aniket-bapat <bapataniket007@gmail.com>
2024-05-14 21:49:05 +0530

    Update pj1.py
    day 1

--- pj1.py
+++ pj1.py
@@ -1,1 +1,47 @@
 print("WELCOME TO MY COMPUTER QUIZ\n")
+
+playing=input("WANNA PLAY ???? \n")
+playing=playing.strip().lower()
+
+if playing!="yes":
+    quit()
+
+score=0
+
+answer=input("What does CPU stands for?\n").lower().strip()
+
+
+if answer=="central processing unit":
+    print("Correct!!!")
+    score+=1
+else:
+    print("Wrong!!!")
+
+answer=input("What does GPU stands for?\n").lower().strip()
+
+
+if answer=="graphic processing unit":
+    print("Correct!!!")
+    score+=1
+else:
+    print("Wrong!!!")
+
+answer=input("What does html stands for?\n").lower().strip()
+
+
+if answer=="hyper text markup language":
+    print("Correct!!!")
+    score+=1
+else:
+    print("Wrong!!!")
+
+answer=input("What does CPU stands for?\n").lower().strip()
+
+
+if answer=="central processing unit":
+    print("Correct!!!")
+    score+=1
+else:
+    print("Wrong!!!")
+
+print("YOU GOT " + str(score)+ " QUSETIONS CORRECT!!")
