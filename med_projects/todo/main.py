072504a2541b5bf4638070a304be1af266c5587c
Aniket-bapat <bapataniket007@gmail.com>
2024-05-17 10:32:15 +0530

    Update main.py

--- med_projects/todo/main.py
+++ med_projects/todo/main.py
@@ -1,58 +1,70 @@
-
 def get_todo():
-    todos=[]
-    with open("todos.txt", "r") as f:
-        for i in f:
-            todos.append(i.strip())
+    todos = []
+    try:
+        with open("todos.txt", "r") as f:
+            for i in f:
+                todos.append(i.strip())
+    except FileNotFoundError:
+        pass  # If the file doesn't exist, we start with an empty list
     return todos
-    
-def append_todo(todos):
+
+def append_todo(todo):
+    with open("todos.txt", "a") as f:
+        f.write(todo + '\n')  # Append the new todo item to the file
+
+def write_todos(todos):
     with open("todos.txt", "w") as f:
-        for i in todos:
-            f.write(i.strip()+"\n")
+        for todo in todos:
+            f.write(todo + '\n')  # Write each todo item to the file
 
 while True:
-    title = input("enter the choice\n")
+    title = input("Enter the choice (type 'exit' to quit):\n")
     title = title.strip()
     
     if title.startswith("add"):
-        todo = title[4:]
-        todos=get_todo()
-        todos.append(todo + '\n')
-        append_todo(todos)
+        todo = title[4:].strip()  # Get the todo item text after "add"
+        append_todo(todo)  # Append the new todo to the file
+        print("Todo added.")
 
     elif title.startswith("show"):
         todos = get_todo()
         for n, i in enumerate(todos):
-            i = i.strip('\n')
-            n = n + 1
-            print(n, "-", i)
+            print(f"{n + 1} - {i}")  # Print the todos with numbering
 
     elif title.startswith("edit"):
         try:
-            n = int(title[5:])
-            n = n - 1
-            todos=get_todo()
-            todos[n] = input("enter the new todo")
-            append_todo()
-
+            n = int(title[5:]) - 1  # Get the index of the todo to edit
+            todos = get_todo()
+            if 0 <= n < len(todos):
+                new_todo = input("Enter the new todo: ").strip()
+                todos[n] = new_todo  # Update the todo item at the specified index
+                write_todos(todos)  # Write the updated list back to the file
+                print("Todo updated.")
+            else:
+                print("Invalid todo number.")
         except ValueError:
-            print("Enter a number following your command")
-            continue
+            print("Please enter a valid todo number.")
+        except IndexError:
+            print("Invalid todo number.")
 
     elif title.startswith("done"):
         try:
-            n = int(title[5:])
-            todos=get_todo()
-            todos.pop(n - 1)
-            append_todo(todos)
-            
-        except ValueError or IndexError:
-            print("Enter a number following your command")
-            continue
-    elif title.startswith("exit"):
-        print("byee!!")
+            n = int(title[5:]) - 1  # Get the index of the todo to remove
+            todos = get_todo()
+            if 0 <= n < len(todos):
+                todos.pop(n)  # Remove the todo item at the specified index
+                write_todos(todos)  # Write the updated list back to the file
+                print("Todo marked as done and removed.")
+            else:
+                print("Invalid todo number.")
+        except ValueError:
+            print("Please enter a valid todo number.")
+        except IndexError:
+            print("Invalid todo number.")
+
+    elif title.lower() == "exit":
+        print("Bye!")
         break
 
     else:
-        print("try again biach ")
+        print("Invalid choice. Please enter 'add', 'show', 'edit', 'done', or 'exit'.")
